13025df84962c6414fe0b8335e83b6852782575c
dogfoot <wownsdl12@naver.com>
2024-07-12 07:48:37 +0900

    add build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-// svelte.config.js
+// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-
-export default {
+import { vitePreprocess } from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // preprocess 추가
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    })
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
   }
 };
+
+export default config;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  base: '/',  // 루트 경로로 설정
 };
 
 export default config;
