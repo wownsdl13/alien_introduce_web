3ac3ea6b58420efe02e9f359af8775f2029c34de
dogfoot <wownsdl12@naver.com>
2024-07-12 08:07:20 +0900

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // preprocess 추가
   preprocess: vitePreprocess(),
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
