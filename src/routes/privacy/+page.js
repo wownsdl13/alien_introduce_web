e9fdc6f08fb083efc566c20e729149540fc56205
dogfoot <wownsdl12@naver.com>
2024-08-28 15:51:29 +0900

    delete preload

--- src/routes/privacy/+page.js
+++ src/routes/privacy/+page.js
@@ -1,7 +1,11 @@
-export const prerender = false;
+export async function load({ fetch, url, stuff }) {
+    let licenseType = '';
 
-export async function load({ fetch, url }) {
-    const licenseType = url.searchParams.get("licenseType");
+    if (typeof window !== 'undefined') {
+        // 클라이언트 측에서만 URL 검색 매개변수에 접근
+        licenseType = url.searchParams.get("licenseType");
+    }
+
     return {
         licenseType
     };
